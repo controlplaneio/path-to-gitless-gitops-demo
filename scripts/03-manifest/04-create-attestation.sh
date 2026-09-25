d89498fa0c0cf78344eea606eefd082263a608b5
Eugene Davis <eugene.davis@control-plane.io>
2026-09-25 13:37:16 +0200

    fix: truncated script

--- scripts/03-manifest/04-create-attestation.sh
+++ scripts/03-manifest/04-create-attestation.sh
@@ -2,12 +2,12 @@
 set -o pipefail
 set -o xtrace
 
-echo "3. Create test attestation"
-echo "3.1. Deploying to test cluster..."
+echo "Create test attestation"
+echo "Deploying to test cluster..."
 kubectl apply -f deploy-oci.yaml
 kubectl wait --for=condition=available deployment/oci-deploy --timeout=60s
 
-echo "3.2. Running integration test..."
+echo "Running integration test..."
 kubectl port-forward deploy/oci-deploy 8000:8000 > /dev/null 2>&1 &
 PF_PID=$!
 
@@ -23,7 +23,7 @@ kill $PF_PID
 if [ "$HTTP_STATUS" == "200" ]; then
   echo "Test passed! Generating test predicate..."
   
-  cat <<TOF > test-result.json
+  cat << "EOF" > test-result.json
 {
   "result": "PASSED",
   "configuration": [
@@ -39,4 +39,31 @@ if [ "$HTTP_STATUS" == "200" ]; then
   "warnedTests": [],
   "failedTests": []
 }
-TOF
+EOF
+
+echo "Attaching in-toto attestation to the registry..."
+
+
+cosign attest \
+  --key openbao://gitless-gitops \
+  --type "https://in-toto.io/attestation/test-result/v0.1" \
+  --predicate test-result.json \
+  --allow-http-registry=true \
+  --yes \
+  "${IMAGE_REPO}@${DIGEST}"
+
+echo "Pipeline complete! Signature and Attestation pushed to ${IMAGE_REPO}."
+oras discover "${IMAGE_REPO}@${DIGEST}"
+
+else
+echo "[ERROR] Test failed with HTTP $HTTP_STATUS"
+exit 1
+fi
+
+
+echo "[INFO] 3.4. Verifying attestation"
+cosign verify-attestation \
+--key openbao://gitless-gitops \
+--type "https://in-toto.io/attestation/test-result/v0.1" \
+--allow-http-registry=true \
+"${IMAGE_REPO}@${DIGEST}" | jq '.payload | @base64d | fromjson'
